cca93ed71864c335baa1bedb983b40ddb4737b5e
kolesnikovalv718-hue <kolesnikovalv718@gmail.com>
2026-04-04 23:30:44 +0500

    1_streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 
-# ---------------------------
+# -------------------------
 # Путь к CSV
 # ---------------------------
 file_path = "blooms_dataset.csv"
