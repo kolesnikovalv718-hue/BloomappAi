fa98f0649a432e370c344ffc6ff096e47b4624e4
kolesnikovalv718-hue <kolesnikovalv718@gmail.com>
2026-04-05 00:35:08 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,15 @@
+import streamlit as st
+
+# Настройка страницы
+st.set_page_config(page_title="Bloom App", layout="wide")
+
+# Выбор страницы через Sidebar
+page = st.sidebar.radio(
+    "Выберите страницу:",
+    ["Редактор задач", "Обучение модели", "Статистика"]
+)
+
+# Импорт соответствующей страницы
 if page == "Редактор задач":
     import pages.tasks_editor_1 as tasks_editor
 elif page == "Обучение модели":
