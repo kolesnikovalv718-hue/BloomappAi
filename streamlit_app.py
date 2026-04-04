503e2992494fbe23c171d6c0a4bc6d5758db5a87
kolesnikovalv718-hue <kolesnikovalv718@gmail.com>
2026-04-04 23:46:37 +0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,10 @@ st.set_page_config(page_title="Bloom App", layout="wide")
 page = st.sidebar.radio("Выберите страницу:", ["Редактор задач", "Обучение модели", "Статистика"])
 
 if page == "Редактор задач":
-    import pages.1_task as tasks_editor
+    import pages.tasks_editor_1 as tasks_editor
 elif page == "Обучение модели":
-    import pages.2_train_model as train_model
+    import pages.train_model_2 as train_model
 elif page == "Статистика":
-    import pages.3_statistics as statistics
+    import pages.statistics_3 as statistics
+    
+
