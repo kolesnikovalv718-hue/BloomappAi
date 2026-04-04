c689b771f10c41ab8107041b096ed554fc1cc8f7
kolesnikovalv718-hue <kolesnikovalv718@gmail.com>
2026-04-05 00:23:35 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
-
-import os
-
-# Создать папку pages, если её нет
-if not os.path.exists("pages"):
-    os.makedirs("pages")
-
-# Создать пустой __init__.py, если его нет
-init_file = os.path.join("pages", "__init__.py")
-if not os.path.exists(init_file):
-    with open(init_file, "w", encoding="utf-8") as f:
-        f.write("# Пакет pages")
+if page == "Редактор задач":
+    import pages.tasks_editor_1 as tasks_editor
+elif page == "Обучение модели":
+    import pages.train_model_2 as train_model
+elif page == "Статистика":
+    import pages.statistics_3 as statistics
