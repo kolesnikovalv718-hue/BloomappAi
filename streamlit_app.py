45cf1c510afe1f4f8659dd1a5b53e2004d496058
kolesnikovalv718-hue <kolesnikovalv718@gmail.com>
2026-04-05 07:36:45 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 st.set_page_config(page_title="Меню", layout="wide")
 
-page = st.sidebar.selectbox("Выбор", ["Главная", "Редактор задач", "Обучение модели"])
+page = st.sidebar.selectbox(
+    "Выбор", 
+    ["Главная", "Редактор задач", "Обучение модели", "Статистика модели"]
+)
 
 # вывод главной сразу
 if page == "Главная":
     st.title("Главная")
     st.write("Выбери задание слева")
 
-# импорт и запуск заданий только после выбора
+#запуск заданий только после выбора
 elif page == "Редактор задач":
     import task1
     task1.run()
@@ -16,3 +19,7 @@ elif page == "Редактор задач":
 elif page == "Обучение модели":
     import task2
     task2.run()
+
+elif page == "Статистика модели":
+    import task3
+    task3.run()
