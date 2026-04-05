803933ea92fe7cd7a13de1dee2b1fda9626fc1df
kolesnikovalv718-hue <kolesnikovalv718@gmail.com>
2026-04-05 06:27:12 +0500

    Create menu01.py

---
+++ menu01.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import task1
+import task2
+
+page = st.sidebar.selectbox("Выбор", ["Задание 1", "Задание 2"])
+
+if page == "Задание 1":
+    task1.run()
+
+if page == "Задание 2":
+    task2.run()
