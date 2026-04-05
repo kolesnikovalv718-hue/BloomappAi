23069a51aa1f196611dd69bc80693667f262163b
kolesnikovalv718-hue <kolesnikovalv718@gmail.com>
2026-04-05 06:59:52 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,13 @@ import streamlit as st
 import task1
 import task2
 
+import streamlit as st
+import task1
+import task2
+
 page = st.sidebar.selectbox("Выбор", ["Задание 1", "Задание 2"])
 
 if page == "Задание 1":
     task1.run()
-if page == "Задание 2":
+elif page == "Задание 2":
     task2.run()
